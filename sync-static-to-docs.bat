6353a915016d82bbf47111f3b8c5cf9e473d186a
EduCode <patrick.ell2@googlemail.com>
2026-02-16 13:52:17 +1100

    Add files via upload

--- sync-static-to-docs.bat
+++ sync-static-to-docs.bat
@@ -10,6 +10,8 @@ if not exist "%DOCS%" mkdir "%DOCS%"
 copy /Y "%ROOT%index.html" "%DOCS%index.html" >nul
 copy /Y "%ROOT%public\*.html" "%DOCS%\" >nul
 copy /Y "%ROOT%public\*.js" "%DOCS%\" >nul
+REM Copy comprehension test HTML files from root (e.g. NAPLAN 2016 Reading Comprehension.html) so they appear on the site
+for %%f in ("%ROOT%*Comprehension*.html" "%ROOT%*Reading*.html") do copy /Y "%%f" "%DOCS%\" >nul 2>nul
 
-echo Synced index.html and public\* to docs\
+echo Synced index.html, public\*, and root comprehension papers to docs\
 pause
